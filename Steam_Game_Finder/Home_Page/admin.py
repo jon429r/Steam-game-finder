6b97495e678d0ceae3053affb1e687620414578c
jon429r <jonathanday088@gmail.com>
2023-10-27 16:57:57 -0400

    Adding to the webpage

--- Steam_Game_Finder/Home_Page/admin.py
+++ Steam_Game_Finder/Home_Page/admin.py
@@ -1,5 +1,5 @@
 
-'''yourapp/admin.py:
+'''HomePage/admin.py:
 You can register your app's models in this file to make them available in the Django admin interface. 
 This is where you define how data is presented and managed in the admin panel.
 '''
--- Steam_Game_Finder/Home_Page/views.py
+++ Steam_Game_Finder/Home_Page/views.py
@@ -1,6 +1,9 @@
-'''yourapp/views.py:
+'''HomePage/views.py:
 This file contains view functions, which handle HTTP requests and return HTTP responses. 
 You define how data is presented to the user in these functions.'''
 from django.shortcuts import render
 
-# Create your views here.
+##create your views here
+
+def home_page_view(request):
+    return render(request, 'Home_Page/Home_Page.html')
