a2029287e29e38ea8f695d22ffcf748628f02910
Newbic <29502356+Newbic@users.noreply.github.com>
2023-11-19 14:07:14 -0500

    Added another fun query about devs

---
+++ Steam_Game_Finder/sql_queries/developers_by_reception.sql
@@ -0,0 +1,16 @@
+-- SQLite
+-- list developers ranked by the average reception of their games
+SELECT Developer, round(avg(Reception), 4) as Dev_Reception, count(*) as Num_Games
+FROM GameDeveloper d INNER JOIN (
+    SELECT AppID, (Positive * 1.0)/(Positive + Negative) as Reception
+    FROM Game
+    -- exclude non-game items
+    WHERE AppID NOT IN (
+        SELECT AppID
+        FROM GameTag
+        WHERE tag = "Software"
+		)
+	) g on d.AppID = g.AppID
+GROUP BY Developer
+HAVING Num_Games > 3 AND Dev_Reception < .9  -- minimum number of games developed and bound on reception
+ORDER BY Dev_Reception DESC, Num_Games DESC
