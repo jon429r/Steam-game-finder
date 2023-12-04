7846545ba050a8abdfa1c19ded371ca5a36f003c
Newbic <29502356+Newbic@users.noreply.github.com>
2023-12-03 20:19:08 -0500

    added recommendation search to calls. corrected conditionals in calls.

--- Steam_Game_Finder/Backend/search_procedures/search_procedure_calls.py
+++ Steam_Game_Finder/Backend/search_procedures/search_procedure_calls.py
@@ -14,13 +14,13 @@ def call_procedure(procedure_name, parameters):
         results = search_functions.language_search(parameters)
     elif procedure_name == "Developer":
         results = search_functions.games_by_developer_search(parameters)
-    elif procedure_name == "games_by_publisher_search":
+    elif procedure_name == "Publisher":
         results = search_functions.games_by_publisher_search(parameters)
     elif procedure_name == "reception_search":
         results = search_functions.reception_search(parameters)
     elif procedure_name == "age_rating_search":
         results = search_functions.age_rating_search(parameters)
-    elif procedure_name == "devolopers_by_reception_search":
+    elif procedure_name == "Devoloper_By_Reception":
         results = search_functions.devolopers_by_reception_search(parameters)
     elif procedure_name == "Genre":
         results = search_functions.genre_search(parameters)
@@ -28,6 +28,8 @@ def call_procedure(procedure_name, parameters):
         results = search_functions.tag_search(parameters)
     elif procedure_name == "category_search":
         results = search_functions.category_search(parameters)
+    elif procedure_name == "Recommendation":
+        results = search_functions.recomendation_search(parameters)
     else:
         results = "Invalid procedure name."
 
